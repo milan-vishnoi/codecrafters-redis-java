cb941798c58cfe24d9f5308e26351b64698223fa
Milan Vishnoi <milan.vish10@gmail.com>
2024-03-29 19:17:50 +0530

    Refactored and reformatted the code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,172 +8,150 @@ import java.util.HashMap;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-   // Log statements can be printed as below, they will be visible when running tests.
+    // Log statements can be printed as below, they will be visible when running
+    // tests.
     System.out.println("Logs from your program will appear here!");
 
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    serverSocket = new ServerSocket(port);
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // // ensures that we don't run into 'Address already in use' errors
+    serverSocket.setReuseAddress(true);
+    // // Wait for connection from client.
+    while (true) {
+      clientSocket = serverSocket.accept();
+      final Socket sc = clientSocket;
+      new Thread(() -> {
+        try {
+          handleCommand(sc);
+        } catch (Exception e) {
+          System.out.println("Received:" + e.getMessage());
+        }
+      }).start();
+    }
 
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //      // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-    //      // Wait for connection from client.
-          while(true)
-          {
-            clientSocket = serverSocket.accept();
-            final Socket sc = clientSocket;
-            new Thread(() -> {
-              try{
-                handleCommand(sc);
-              }catch(Exception e){
-                System.out.println("Received:"+e.getMessage());
-              }
-            }).start();
-          }
-        
   }
 
   private static void handleCommand(Socket clientSocket) {
     OutputStreamWriter out = null;
     BufferedReader in = null;
-    try
-    {
-    out = new OutputStreamWriter(clientSocket.getOutputStream());
-    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    String line = null;
-    line = in.readLine();
-    if(line.startsWith("*"))
-    {
-      int noOfLines = Integer.parseInt(line.substring(1));
-      System.out.println("Number of lines:"+ noOfLines);
-      HashMap<String,String> values = new HashMap<>();
-      HashMap<String,Long> validDuration = new HashMap<>();
-      // int i=0;
-      while(true)//for(int i = 0 ; i< noOfLines ; i++)
-      {   
-          if((line = in.readLine()).startsWith("$"))
-          { 
+    try {
+      out = new OutputStreamWriter(clientSocket.getOutputStream());
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String line = null;
+      line = in.readLine();
+      if (line.startsWith("*")) {
+        int noOfLines = Integer.parseInt(line.substring(1));
+        System.out.println("Number of lines:" + noOfLines);
+        HashMap<String, String> values = new HashMap<>();
+        HashMap<String, Long> validDuration = new HashMap<>();
+        while (true) {
+          if ((line = in.readLine()).startsWith("$")) {
             int commandLength = Integer.parseInt(line.substring(1));
             String command = in.readLine().substring(0, commandLength);
-            // i++;
             if (command.equalsIgnoreCase("PING"))
               out.write("+PONG\r\n");
             else if (command.equalsIgnoreCase("ECHO"))
-            handleEchoCommand(out, in);// i = handleEchoCommand(out, in, i);
+              handleEchoCommand(out, in);
             else if (command.equalsIgnoreCase("SET"))
-            handleSetCommand(out, in, values, validDuration, noOfLines); // i = handleSetCommand(out, in, values, validDuration, i, noOfLines);
+              handleSetCommand(out, in, values, validDuration, noOfLines);
             else if (command.equalsIgnoreCase("GET"))
-            handleGetCommand(out, in, values, validDuration);// i = handleGetCommand(out, in, values, validDuration, i);
+              handleGetCommand(out, in, values, validDuration);
           }
-          // System.out.println("Came back with i="+i);
           out.flush();
+        }
       }
-    }
-    
-    } catch(IOException ex)
-    {
-      System.out.println("Received:"+ex.getMessage());
-    }finally{
-      try{
-        if(clientSocket != null)
-        {
-          if(out != null)
-          out.close();
-  
-          if(in != null)
-          in.close();
-  
+
+    } catch (IOException ex) {
+      System.out.println("Received:" + ex.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          if (out != null)
+            out.close();
+
+          if (in != null)
+            in.close();
+
           clientSocket.close();
         }
-      }catch(IOException e)
-      {
-        System.out.println("Received:"+e.getMessage());
+      } catch (IOException e) {
+        System.out.println("Received:" + e.getMessage());
       }
-      
+
     }
-    
+
   }
 
-  private static void handleEchoCommand(OutputStreamWriter out, BufferedReader in) throws IOException 
-  {
+  private static void handleEchoCommand(OutputStreamWriter out, BufferedReader in) throws IOException {
     String line;
-    if((line = in.readLine()).startsWith("$"))
-     { 
-        int argumentLength = Integer.parseInt(line.substring(1));
-        String argument = in.readLine().substring(0,argumentLength);
-        // i++;
-        out.write("$"+argumentLength+"\r\n"+argument+"\r\n");
-     }
-    // return i;
+    if ((line = in.readLine()).startsWith("$")) {
+      int argumentLength = Integer.parseInt(line.substring(1));
+      String argument = in.readLine().substring(0, argumentLength);
+      out.write("$" + argumentLength + "\r\n" + argument + "\r\n");
+    }
   }
 
   private static void handleGetCommand(OutputStreamWriter out, BufferedReader in, HashMap<String, String> values,
-  HashMap<String,Long> validDuration)
-      throws IOException 
-      {
+      HashMap<String, Long> validDuration)
+      throws IOException {
     String line;
-    if ((line = in.readLine()).startsWith("$")) 
-    {
+    if ((line = in.readLine()).startsWith("$")) {
       int keyLength = Integer.parseInt(line.substring(1));
       String key = in.readLine().substring(0, keyLength);
-      // i++;
       String value = values.get(key);
-      if(value == null || value.isEmpty())
-      value = "-1";
-      else if(validDuration.get(key)< System.currentTimeMillis())
-      {
+      if (value == null || value.isEmpty())
+        value = "-1";
+      else if (validDuration.get(key) < System.currentTimeMillis()) {
         value = "-1";
         values.remove(key);
         validDuration.remove(key);
       }
-      
-      if(value.equals("-1"))
-      out.write("$" + value+ "\r\n");
+
+      if (value.equals("-1"))
+        out.write("$" + value + "\r\n");
       else
-      out.write("$" + value.length() + "\r\n" + value + "\r\n");
+        out.write("$" + value.length() + "\r\n" + value + "\r\n");
     }
-    // return i;
   }
 
-  private static void handleSetCommand(OutputStreamWriter out, BufferedReader in, HashMap<String, String> values, 
-  HashMap<String,Long> validDuration, int noOfLines)
-      throws IOException 
-      {
-        int i = 1;
+  private static void handleSetCommand(OutputStreamWriter out, BufferedReader in, HashMap<String, String> values,
+      HashMap<String, Long> validDuration, int noOfLines)
+      throws IOException {
+    int argumentNumber = 1;
     String line;
-    if ((line = in.readLine()).startsWith("$")) 
-    {
+    if ((line = in.readLine()).startsWith("$")) {
       int keyLength = Integer.parseInt(line.substring(1));
       String key = in.readLine().substring(0, keyLength);
-      i++;
-      if ((line = in.readLine()).startsWith("$")) 
-      {
+      argumentNumber++;
+      if ((line = in.readLine()).startsWith("$")) {
         int valueLength = Integer.parseInt(line.substring(1));
         String value = in.readLine().substring(0, valueLength);
-        i++;
+        argumentNumber++;
         values.put(key, value);
-        validDuration.put(key,Long.MAX_VALUE);
-        System.out.println("Value of i = "+i);
-        if( i < noOfLines && (line=in.readLine()).startsWith("$"))
-        {
-            System.out.println("Within if:"+line);
-            if((line=in.readLine()).toUpperCase().contains("PX"))
-            { i++;
-              if((line = in.readLine()).startsWith("$"))
-              {
-                int expiryLength = Integer.parseInt(line.substring(1));
-                int expiry = Integer.parseInt(in.readLine().substring(0, expiryLength));
-                i++;
-                validDuration.put(key, System.currentTimeMillis()+expiry);
-              }
-            }
-        }
-        System.out.println("Before out.write");
+        validDuration.put(key, Long.MAX_VALUE);
+        handleExpiry(in, validDuration, noOfLines, argumentNumber, key);
         out.write("+OK\r\n");
       }
     }
-    // return i;
+  }
+
+  private static void handleExpiry(BufferedReader in, HashMap<String, Long> validDuration, int noOfLines,
+      int argumentNumber,
+      String key) throws IOException {
+    String line;
+    if (argumentNumber < noOfLines && (line = in.readLine()).startsWith("$")) {
+      if ((line = in.readLine()).toUpperCase().contains("PX")) {
+        argumentNumber++;
+        if ((line = in.readLine()).startsWith("$")) {
+          int expiryLength = Integer.parseInt(line.substring(1));
+          int expiry = Integer.parseInt(in.readLine().substring(0, expiryLength));
+          argumentNumber++;
+          validDuration.put(key, System.currentTimeMillis() + expiry);
+        }
+      }
+    }
   }
 }
